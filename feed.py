d1e4b2564b185ede314b427fe85b835b8188ae86
Cong Yuan <congyuanhng@gmail.com>
2024-03-28 10:33:40 +0800

    Modified Feed and added GitHub Actions workflow for generating podcast feeds

--- feed.py
+++ feed.py
@@ -22,9 +22,7 @@ xml_tree.SubElement(channel_element, "format").text = yaml_data["format"]
 xml_tree.SubElement(channel_element, "subtitle").text = yaml_data["subtitle"]
 xml_tree.SubElement(channel_element, "itunes:author").text = yaml_data["author"]
 xml_tree.SubElement(channel_element, "description").text = yaml_data["description"]
-xml_tree.SubElement(
-    channel_element, "itunes:image", {"href": link_prefix + yaml_data["image"]}
-)
+xml_tree.SubElement(channel_element, "itunes:image", {"href": link_prefix + yaml_data["image"]})
 xml_tree.SubElement(channel_element, "language").text = yaml_data["language"]
 xml_tree.SubElement(channel_element, "link").text = link_prefix
 
